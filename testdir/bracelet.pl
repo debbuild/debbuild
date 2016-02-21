a15efcd473dbc74cd76e74fa7c3dca5a03cc51b6
Andreas Scherer <andreas_github@freenet.de>
2016-02-21 15:54:06 +0100

    Extract the generic option replacement routine.
    How can we process '%{-o}ptions' on multiple levels? E.g.,
    '%{uncompress:...}' might itself have its own set of options, one level
    higher than '%{__scm_apply_patch}' handled in this experiment.
    
    Simply putting 'replace_options()' _after_ each recursive 'for'-loop
    does _not_ work as expected (yet). This results in some weird mixture of
    replacements.

--- testdir/bracelet.pl
+++ testdir/bracelet.pl
@@ -27,30 +27,38 @@ foreach ( map { /{(.+)}/ } grep { /{.+}/ } bracelet() ) {
 
   foreach ( map { /{(.+)}/ } grep { /{.+}/ } bracelet() ) {
 
-    while (my ($option,$repl) = map { /{-(\w):\s*(.+)}/ } bracelet() ) {
-      print Dumper('+++',$option,$repl);
-      (my $result = $options{$option} ? $repl : '') =~ s/%{-(\w)\*}/$options{$1}/g;
-      $repl =~ s/([*{}])/\\$1/g; # mask Perlish special characters
-print "---$_(".pos.")---\n";
-      s/%{-$option:\s*$repl}/$result/g;
-print "+++$_+++\n";
-      $macro =~ s/%{-$option:\s*$repl}/$result/g;
-    }
-    pos = 0; # reset to start of $_
-    while (my ($option) = map { /{-(\w)(?:\*)?}/ } bracelet() ) {
-      print Dumper('+++',$option);
-      my $result = $options{$option} ? "-$option" : '';
-print "---$_(".pos.")---\n";
-      s/%{-$option}/$result/g;
-      $macro =~ s/%{-$option}/$result/g;
-print "---$_---\n";
-      s/%{-($option)\*}/$options{$1}/g;
-      $macro =~ s/%{-($option)\*}/$options{$1}/g;
-print "+++$_+++\n";
-    }
+    replace_options();
+
   }
+
 }
 
 print "\n>>>>>>\n".Dumper($macro);
 
 exit 0;
+
+## replace_options()
+# Expand all '%{-o:ptions}' and '%{-o}ptions' in '$_'.
+sub replace_options {
+  while (my ($option,$repl) = map { /{-(\w):\s*(.+)}/ } bracelet() ) {
+    print Dumper('+1+',$option,$repl);
+    (my $result = $options{$option} ? $repl : '') =~ s/%{-(\w)\*}/$options{$1}/g;
+    $repl =~ s/([*{}])/\\$1/g; # mask Perlish special characters
+print "-1-$_(".pos.")-1-\n";
+    s/%{-$option:\s*$repl}/$result/g;
+print "+1+$_+1+\n";
+    $macro =~ s/%{-$option:\s*$repl}/$result/g;
+  }
+  pos = 0; # reset to start of $_
+  while (my ($option) = map { /{-(\w)(?:\*)?}/ } bracelet() ) {
+    print Dumper('+2+',$option);
+    my $result = $options{$option} ? "-$option" : '';
+print "-2-$_(".pos.")-2-\n";
+    s/%{-$option}/$result/g;
+    $macro =~ s/%{-$option}/$result/g;
+print "-2-$_-2-\n";
+    s/%{-($option)\*}/$options{$1}/g;
+    $macro =~ s/%{-($option)\*}/$options{$1}/g;
+print "+2+$_+2+\n";
+  }
+} # end replace_options()
