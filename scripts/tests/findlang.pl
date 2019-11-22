22e12ad3ae714f8db767daa0889f65182e2179d7
Neal Gompa <ngompa13@gmail.com>
2019-11-22 12:04:46 -0500

    Import find-lang.pl script from Mageia
    This version of the find-lang script used to be used in Mageia
    until Mageia 6. However, the shell version is hard to read and
    we already have a Perl dependency with debbuild itself, so this
    variant is a good starting point for debbuild's own implementation.

---
+++ scripts/tests/findlang.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+
+# Copyright (C) 2007-2010 Mandriva, S.A.
+# Copyright (C) 2010-2017 Mageia
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use strict;
+use warnings;
+
+use Test::More qw(no_plan);
+
+my $pwd = `pwd`;
+chomp($pwd);
+my $testarbo = "$pwd/find-lang-root";
+
+
+my %files = (
+    # file => [ lang, is_%dir ]
+    '/usr/share/man/fr' => [ 'fr', 1 ],
+    '/usr/share/man/fr/man1' => [ 'fr', 1 ],
+    '/usr/share/man/fr/man1/lang.1.*' => [ 'fr', 0 ],
+    '/usr/share/locale/pt_BR' => [ 'pt_BR', 1 ],
+    '/usr/share/gnome/help/lang/pt_BR' => [ 'pt_BR', 1 ],
+    '/usr/share/gnome/help/lang/pt_BR/any.html' => [ 'pt_BR', 0 ],
+    '/usr/share/gnome/help/lang/en_GB' => [ 'en_GB', 1 ],
+    '/usr/share/gnome/help/lang/en_GB/any.html' => [ 'en_GB', 0 ],
+    '/usr/share/gnome/help/lang' => [ '', 1 ],
+    '/usr/share/locale/pt_BR/LC_MESSAGES' => [ 'pt_BR', 1 ],
+    '/usr/share/locale/pt_BR/LC_MESSAGES/lang.mo' => [ 'pt_BR', 0 ],
+);
+
+system("/usr/bin/perl find-lang.pl $testarbo lang --with-man --with-gnome") and die "can't run find-lang $@";
+
+open(my $h, '<', 'lang.lang') or die "can't open lang.lang";
+
+while (my $line = <$h>) {
+    chomp($line);
+    $line =~ /svn/ and next;
+    my ($file) = $line =~ / ([^ ]+)$/;
+    my ($lang) = $line =~ /%lang\(([^\)]+)\)/;
+    $lang ||= '';
+    my $dir = $line =~ /%dir/;
+    $dir ||= 0;
+    print STDERR "$file\n";
+    is($lang, $files{$file}[0], "lang is find");
+    is($dir, $files{$file}[1], "%dir is properly set if need");
+}
+
