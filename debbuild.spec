debbuild/debbuild

commit 90eb8c1b60505cd47e3874a880df04afb6c6b623

spec: Add subpackage for Lua macro support
Now that we have optional support for Lua-based RPM macros,
we should provide a way to make sure people can use it from
within build systems easily enough.

debbuild.spec
@@ -60,11 +60,26 @@ Recommends:     unzip
 Recommends:     zip
 Recommends:     zstd
 
+Recommends:     %{name}-lua-support
+
 %description
 debbuild attempts to build Debian-friendly semi-native packages from
 RPM spec files, RPM-friendly tarballs, and RPM source packages
 (.src.rpm files).  It accepts most of the options rpmbuild does, and
 should be able to interpret most spec files usefully.
+
+%package lua-support
+Summary:        Lua macro support for debbuild
+Requires:       %{name} = %{version}-%{release}
+%if "%{_vendor}" == "debbuild"
+Requires:       liblua-api-perl
+%else
+Requires:       perl(Lua::API)
+%endif
+
+%description lua-support
+This package adds the dependencies to support RPM macros
+written the Lua programming language.
 
 %prep
 # Steps to unpack and patch source as needed
@@ -88,6 +103,9 @@ make
 %{_mandir}/man8/*
 %{debconfigdir}/
 %{_sysconfdir}/debbuild/
+
+%files lua-support
+# Empty metapackage
 
 %changelog
 * Mon Dec 28 2020 Neal Gompa <ngompa13@gmail.com>

commit a5e9590d31a966710b7865d6e5360fca3ad49efe

spec: Add changelog entry for Lua support subpackage
This was forgotten when adding the subpackage.

Fixes: 90eb8c1b60505cd47e3874a880df04afb6c6b623

debbuild.spec
@@ -108,6 +108,9 @@ make
 # Empty metapackage
 
 %changelog
+* Sat Feb 19 2022 Neal Gompa <ngompa13@gmail.com>
+- Add subpackage for pulling in support for RPM macros written in Lua
+
 * Mon Dec 28 2020 Neal Gompa <ngompa13@gmail.com>
 - Quote words on both sides of string comparisons for RPM 4.16+ compatibility
 